e8bf64f2248f77185ca5c1babc56135b797d7851
Rachinder3 <rachindersingh@gmail.com>
2022-09-14 18:40:37 +0530

    updated the constants

---
+++ src/deepClassifier/constants/__init__.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("configs\config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
+
+
+
+# Testing the script
+#if __name__=='__main__':
+#    print(CONFIG_FILE_PATH)
+    
+    
