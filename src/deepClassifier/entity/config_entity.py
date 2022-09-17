d81d26e8c9350bfa28667e1f1f24d4d328150a63
Rachinder3 <rachindersingh@gmail.com>
2022-09-17 21:28:58 +0530

    Configuration Entity added

---
+++ src/deepClassifier/entity/config_entity.py
@@ -0,0 +1,11 @@
+
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass(frozen=True)   ## frozen = True makes it behave like named tuple,we won't be able to change anything
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir:Path
+    
