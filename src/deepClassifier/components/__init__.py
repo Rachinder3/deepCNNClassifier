a2ce43b3bc2b53a3de4c8ac30d828de5d9794bb2
Rachinder3 <rachindersingh@gmail.com>
2022-09-21 12:49:29 +0530

    Stage 2 added

--- src/deepClassifier/components/__init__.py
+++ src/deepClassifier/components/__init__.py
@@ -1,4 +1,4 @@
 from deepClassifier.components.data_ingestion import DataIngestion
-
+from deepClassifier.components.prepare_base_model import PrepareBaseModel
 
 
---
+++ src/deepClassifier/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,25 @@
+from deepClassifier.config import ConfigurationManager
+from deepClassifier.components import PrepareBaseModel
+from deepClassifier import logger
+
+
+STAGE_NAME = "Prepare Base Model Step"
+
+
+def main():
+    config = ConfigurationManager()
+    prepare_base_model_config = config.get_prepare_base_model_config()
+    prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+    prepare_base_model.get_base_model()
+    prepare_base_model.update_base_model()
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f"{'>>'*30} stage {STAGE_NAME} started {'>>'*30}")
+        main()
+        logger.info(f"{'>>'*30} stage {STAGE_NAME} Completed {'>>'*30}")
+    except Exception as e:
+        logger.info(e)
+
+        raise e
